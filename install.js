77664e754fb96e71d8889171c7b3a1f354a38aa3
John Sánchez Alvarez <esutoraiki@gmail.com>
2022-04-27 02:26:43 -0500

    Correction install

--- install.js
+++ install.js
@@ -2,12 +2,12 @@ const
     fs = require("fs"),
 
     // args = process.argv.slice(2),
-    dest = "./",
-    filesDir = "node_modules/sisass/files/"
+    dest = "../../",
+    filesDir = "files/"
 ;
 
 // Copy files to the project
-fs.cp(filesDir, dest, {recursive: true}, (err, data) => {
+fs.cp(filesDir, dest, {recursive: true, force: false}, (err, data) => {
     if (err) {
         console.error("Error Install");
         console.error(err);
