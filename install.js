f7c48976611d3a153957af3886701e432d72ca84
John Sánchez Alvarez <john.sanchez@wundermanthompson.com>
2023-05-01 15:45:37 -0500

    FEAT(install): Custom installation for sqhtml

--- install.js
+++ install.js
@@ -1,10 +1,23 @@
+let
+    filesDir = ""
+;
+
 const
     arg = require("./arg.js").arg
     fs = require("fs"),
 
     dest = arg.path || "../../",
-    filesDir = (dest === "../../") ? "files/" : "files/assets/"
+    dependents = arg.dep || ""
 ;
+
+switch (dependents) {
+    case "sqhtml":
+        filesDir = "files_sqhtml/"
+    break;
+    default:
+        filesDir = (dest === "../../") ? "files/" : "files/assets/"
+    break;
+}
 
 // Copy files to the project
 fs.cp(filesDir, dest, {recursive: true, force: false}, (err, data) => {
