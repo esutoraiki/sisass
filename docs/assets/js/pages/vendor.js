bdce44237a3b1dc733e957b445dab53e6e72257d
John Sánchez Alvarez <esutoraiki@gmail.com>
2026-03-16 00:37:05 -0500

    DOCS: move vendor documentation outside base

---
+++ docs/assets/js/pages/vendor.js
@@ -0,0 +1,27 @@
+import { contentLoad } from "../core/fn.js";
+import { init_documentation_search } from "../core/search.js";
+
+(function () {
+    "use strict";
+
+    const
+        url_json_vendor = "json/vendor.json",
+
+        NSVendor = (function () {
+            return {
+                content: () => {
+                    contentLoad({
+                        url: url_json_vendor,
+                        complete: function () {
+                            init_documentation_search();
+                        }
+                    });
+                }
+            };
+        }())
+    ;
+
+    window.addEventListener("load", function () {
+        NSVendor.content();
+    });
+}());
