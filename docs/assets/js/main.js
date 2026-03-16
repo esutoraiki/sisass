c24a5b000f6d2c83fa330c0cbb6511f65106dd2a
John Sánchez Alvarez <esutoraiki@gmail.com>
2026-03-16 00:46:34 -0500

    DOCS: show only the active submenu in the documentation

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -5,6 +5,7 @@ import { contentLoad } from "./core/fn.js";
 
     const
         c1 = "active",
+        c2 = "hide",
         url_json_global = "json/global.json",
 
         NSDocumentation = (function () {
@@ -16,10 +17,19 @@ import { contentLoad } from "./core/fn.js";
                             if (id_component === "menu") {
                                 let
                                     s1 = document.getElementsByClassName("documentationpage"),
-                                    s2 = document.getElementById("link_" + s1[0].dataset.link)
+                                    s2 = s1.length > 0 ? s1[0] : null,
+                                    s3 = s2 ? s2.dataset.link : "",
+                                    s4 = s3 !== "" ? document.getElementById("link_" + s3) : null,
+                                    s5 = s3 !== "" ? document.getElementById("submenu_" + s3) : null
                                 ;
 
-                                s2.classList.add(c1);
+                                if (s4) {
+                                    s4.classList.add(c1);
+                                }
+
+                                if (s5) {
+                                    s5.classList.remove(c2);
+                                }
                             }
                         }
                     });
