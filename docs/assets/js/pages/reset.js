345781f535c7108cfe786165a4d2bd8998d1e936
John Sánchez Alvarez <esutoraiki@gmail.com>
2026-06-30 20:28:50 -0500

    DOCS: Update assets and reset page of the documentation

---
+++ docs/assets/js/pages/reset.js
@@ -0,0 +1,43 @@
+import { contentLoad } from "../core/fn.js";
+import { init_hash_navigation } from "../core/hash_navigation.js";
+import { loader } from "../core/page_loader.js";
+import { init_documentation_search } from "../core/search.js";
+
+(function () {
+    "use strict";
+
+    const
+        currentPage = "documentationpage",
+        url_json_reset = "json/reset.json",
+
+
+        NSReset = (function () {
+            return {
+                content: async () => {
+                    loader.register([
+                        "content_ready",
+                        "navigation_ready",
+                        "search_ready"
+                    ]);
+
+                    await contentLoad({
+                        url: url_json_reset
+                    });
+                    loader.set("content_ready", true);
+
+                    await init_hash_navigation();
+                    loader.set("navigation_ready", true);
+
+                    await init_documentation_search({
+                        current_page: currentPage
+                    });
+                    loader.set("search_ready", true);
+                }
+            };
+        }())
+    ;
+
+    window.addEventListener("load", async function () {
+        await NSReset.content();
+    });
+}());
