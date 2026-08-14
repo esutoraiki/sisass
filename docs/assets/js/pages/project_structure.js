05c14021a4b698da2edfbbdec2894a56f6bf5008
John Sánchez Alvarez <esutoraiki@gmail.com>
2026-08-14 01:23:51 -0500

    COMPONENTS(feat): integrate tab_panel and tree viewer into the documentation

--- docs/assets/js/pages/project_structure.js
+++ docs/assets/js/pages/project_structure.js
@@ -4,14 +4,79 @@ import { init_hash_navigation } from "../core/hash_navigation.js";
 import { loader } from "../core/page_loader.js";
 import { init_documentation_search } from "../core/search.js";
 
+/* global TreeConvertJson TreeViewer */
+
 (function () {
     "use strict";
 
     const
         current_page = "documentationpage",
         url_json_project_structure = "json/project_structure.json",
+        url_project_tree = "text/tree",
+        url_icon_file = "img/svg/file.svg",
+        url_icon_folder = "img/svg/folder.svg",
+        url_icon_js = "img/svg/js.svg",
+        url_icon_root = "img/svg/root.svg",
+        url_icon_sass = "img/svg/sass.svg",
 
         NSProjectStructure = (function () {
+            async function init_project_tree() {
+                const tree_node = document.getElementById("project_structure_tree");
+
+                if (!tree_node || typeof TreeConvertJson === "undefined" || typeof TreeViewer === "undefined") {
+                    return;
+                }
+
+                try {
+                    const tree_response = await fetch(url_project_tree);
+
+                    if (!tree_response.ok) {
+                        throw new Error("Unable to load the project tree source.");
+                    }
+
+                    const tree_source = await tree_response.text();
+                    const tree_converter = new TreeConvertJson({
+                        source: tree_source
+                    });
+                    const tree_data = tree_converter.convert();
+                    const tree_viewer = new TreeViewer(tree_node, {
+                        format: "json",
+                        source: tree_data,
+                        title: "Mapa principal del repositorio",
+                        show_icons: true,
+                        folder_icon: url_icon_folder,
+                        file_icon: url_icon_file,
+                        default_icon: url_icon_file,
+                        icon_resolver: function (node) {
+                            const node_name = String(node?.name ?? "").toLowerCase();
+
+                            if (node_name === ".") {
+                                return url_icon_root;
+                            }
+
+                            if (node_name.endsWith(".js")) {
+                                return url_icon_js;
+                            }
+
+                            if (node_name.endsWith(".scss")) {
+                                return url_icon_sass;
+                            }
+
+                            if (node.type === "folder") {
+                                return url_icon_folder;
+                            }
+
+                            return url_icon_file;
+                        }
+                    });
+
+                    tree_viewer.init();
+                } catch (error) {
+                    tree_node.textContent = "No se pudo cargar el mapa principal del repositorio.";
+                    console.error(error);
+                }
+            }
+
             return {
                 content: async () => {
                     loader.register([
@@ -23,6 +88,7 @@ import { init_documentation_search } from "../core/search.js";
                     await contentLoad({
                         url: url_json_project_structure
                     });
+                    await init_project_tree();
                     await init_page_breadcrumb({
                         json_url: url_json_project_structure
                     });
